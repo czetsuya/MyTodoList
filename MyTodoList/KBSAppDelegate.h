279dce432a0613b921c99e5b8ac8a1e36e3a4602
edward.legaspi <czetsuya@gmail.com>
2014-04-17 08:06:43 +0800

    Initial Commit

---
+++ MyTodoList/KBSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KBSAppDelegate.h
+//  MyTodoList
+//
+//  Created by czetsuya on 4/17/14.
+//  Copyright (c) 2014 Kalidad Business Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyTodoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyTodoList
+//
+//  Created by czetsuya on 4/17/14.
+//  Copyright (c) 2014 Kalidad Business Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KBSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KBSAppDelegate class]));
+    }
+}
